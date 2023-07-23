89f7b1488ab9d9f9e000aca72bf5e98b80d70bae
Андрей Брижанёв <myrrvin@MacBook-Pro-Andrej.local>
2023-07-24 00:37:45 +0300

    feat: add homework

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,158 @@
-/**
- * @file Домашка по FP ч. 2
- *
- * Подсказки:
- * Метод get у инстанса Api – каррированый
- * GET / https://animals.tech/{id}
- *
- * GET / https://api.tech/numbers/base
- * params:
- * – number [Int] – число
- * – from [Int] – из какой системы счисления
- * – to [Int] – в какую систему счисления
- *
- * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
- * Ответ будет приходить в поле {result}
- */
- import Api from '../tools/api';
+// /**
+//  * @file Домашка по FP ч. 2
+//  *
+//  * Подсказки:
+//  * Метод get у инстанса Api – каррированый
+//  * GET / https://animals.tech/{id}
+//  *
+//  * GET / https://api.tech/numbers/base
+//  * params:
+//  * – number [Int] – число
+//  * – from [Int] – из какой системы счисления
+//  * – to [Int] – в какую систему счисления
+//  *
+//  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
+//  * Ответ будет приходить в поле {result}
+//  */
+// import Api from "../tools/api";
 
- const api = new Api();
+// const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+// /**
+//  * Я – пример, удали меня
+//  */
+// const wait = (time) =>
+//   new Promise((resolve) => {
+//     setTimeout(resolve, time);
+//   });
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+// const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+//   /**
+//    * Я – пример, удали меня
+//    */
+//   writeLog(value);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+//   api
+//     .get("https://api.tech/numbers/base", {
+//       from: 2,
+//       to: 10,
+//       number: "01011010101",
+//     })
+//     .then(({ result }) => {
+//       writeLog(result);
+//     });
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+//   wait(2500)
+//     .then(() => {
+//       writeLog("SecondLog");
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+//       return wait(1500);
+//     })
+//     .then(() => {
+//       writeLog("ThirdLog");
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+//       return wait(400);
+//     })
+//     .then(() => {
+//       handleSuccess("Done");
+//     });
+// };
+
+// export default processSequence;
+
+import Api from "../tools/api";
+import {
+  __,
+  allPass,
+  andThen,
+  assoc,
+  compose,
+  concat,
+  gt,
+  ifElse,
+  length,
+  lt,
+  mathMod,
+  otherwise,
+  partial,
+  prop,
+  tap,
+  test,
+} from "ramda";
+
+const api = new Api();
+
+const square = (num) => num ** 2;
+const gtTwo = gt(__, 2);
+const ltTen = lt(__, 10);
+
+const thenSquare = andThen(square);
+
+const lengthGreaterThenTwo = compose(gtTwo, length);
+const lengthLowerThenTen = compose(ltTen, length);
+const testOnlyNumbers = test(/^[0-9]+\.?[0-9]+$/);
+const roundStringToNumber = compose(Math.round, Number);
+const modForThreeToString = compose(String, mathMod(__, 3));
+
+const thenModOfThreeToString = andThen(modForThreeToString);
+const thenGetLength = andThen(length);
+
+const validate = allPass([
+  lengthGreaterThenTwo,
+  lengthLowerThenTen,
+  testOnlyNumbers,
+]);
+
+const API_NUMBERS_URL = "https://api.tech/numbers/base";
+const API_ANIMALS_URL = "https://animals.tech/";
+const getApiResult = compose(String, prop("result"));
+
+const assocNumberToBinary = assoc("number", __, { from: 10, to: 2 });
+
+const apiGetNumberBinaryBase = compose(
+  api.get(API_NUMBERS_URL),
+  assocNumberToBinary
+);
+
+const thenGetApiResult = andThen(getApiResult);
+const thenConcatToAnimalsUrl = andThen(concat(API_ANIMALS_URL));
+const thenCallApiWithEmptyParams = andThen(api.get(__, {}));
+
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  const tapLog = tap(writeLog);
+  const thenTapLog = andThen(tapLog);
+  const thenHandleSuccess = andThen(handleSuccess);
+  const otherwiseHandleError = otherwise(handleError);
+
+  const handleValidationError = partial(handleError, ["ValidationError"]);
+
+  const sequenceComposition = compose(
+    otherwiseHandleError,
+    thenHandleSuccess,
+    thenGetApiResult,
+    thenCallApiWithEmptyParams,
+    thenConcatToAnimalsUrl,
+    thenTapLog,
+    thenModOfThreeToString,
+    thenTapLog,
+    thenSquare,
+    thenTapLog,
+    thenGetLength,
+    thenTapLog,
+    thenGetApiResult,
+    apiGetNumberBinaryBase,
+    tapLog,
+    roundStringToNumber
+  );
+
+  const runWithCondition = ifElse(
+    validate,
+    sequenceComposition,
+    handleValidationError
+  );
+  const logAndRunSequence = compose(runWithCondition, tapLog);
+
+  logAndRunSequence(value);
+};
 
 export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,3 +1,127 @@
+import {
+  __,
+  prop,
+  any,
+  dissoc,
+  compose,
+  equals,
+  allPass,
+  countBy,
+  identity,
+  values,
+  complement,
+  propEq,
+  gte,
+} from "ramda";
+
+// /**
+//  * @file Домашка по FP ч. 1
+//  *
+//  * Основная задача — написать самому, или найти в FP библиотеках функции anyPass/allPass
+//  * Эти функции/их аналоги есть и в ramda и в lodash
+//  *
+//  * allPass — принимает массив функций-предикатов, и возвращает функцию-предикат, которая
+//  * вернет true для заданного списка аргументов, если каждый из предоставленных предикатов
+//  * удовлетворяет этим аргументам (возвращает true)
+//  *
+//  * anyPass — то же самое, только удовлетворять значению может единственная функция-предикат из массива.
+//  *
+//  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
+//  */
+
+// // 1. Красная звезда, зеленый квадрат, все остальные белые.
+// export const validateFieldN1 = ({star, square, triangle, circle}) => {
+//     if (triangle !== 'white' || circle !== 'white') {
+//         return false;
+//     }
+//     console.log(star);
+//     return star === 'red' && square === 'green';
+// };
+
+// // 2. Как минимум две фигуры зеленые.
+// export const validateFieldN2 = () => false;
+
+// // 3. Количество красных фигур равно кол-ву синих.
+// export const validateFieldN3 = () => false;
+
+// // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
+// export const validateFieldN4 = () => false;
+
+// // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
+// export const validateFieldN5 = () => false;
+
+// // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
+// export const validateFieldN6 = () => false;
+
+// // 7. Все фигуры оранжевые.
+// export const validateFieldN7 = () => false;
+
+// // 8. Не красная и не белая звезда, остальные – любого цвета.
+// export const validateFieldN8 = () => false;
+
+// // 9. Все фигуры зеленые.
+// export const validateFieldN9 = () => false;
+
+// // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
+// export const validateFieldN10 = () => false;
+
+const getStar = prop("star");
+const getSquare = prop("square");
+const getTriangle = prop("triangle");
+const getCircle = prop("circle");
+
+const getGreen = prop("green");
+const getRed = prop("red");
+const getBlue = prop("blue");
+
+const isWhite = equals("white");
+const isRed = equals("red");
+const isOrange = equals("orange");
+const isGreen = equals("green");
+const isBlue = equals("blue");
+
+const dissocWhite = dissoc("white");
+
+const isStarRed = compose(isRed, getStar);
+const isStarWhite = compose(isWhite, getStar);
+const isStarGreen = compose(isGreen, getStar);
+const isStarNotWhite = complement(isStarWhite);
+const isStarNotRed = complement(isStarRed);
+
+const isSquareWhite = compose(isWhite, getSquare);
+const isSquareOrange = compose(isOrange, getSquare);
+const isSquareGreen = compose(isGreen, getSquare);
+const isNotWhiteSquare = complement(isSquareWhite);
+
+const isTriangleWhite = compose(isWhite, getTriangle);
+const isTriangleGreen = compose(isGreen, getTriangle);
+const isNotWhiteTriangle = complement(isTriangleWhite);
+
+const isCircleWhite = compose(isWhite, getCircle);
+const isCircleBlue = compose(isBlue, getCircle);
+const isCircleGreen = compose(isGreen, getCircle);
+
+const countDupes = compose(countBy(identity), values);
+const countDupesWhitoutWhite = compose(dissocWhite, countDupes);
+
+const numberOfGreenColors = compose(getGreen, countDupes);
+const redEqualsBlue = ({ blue, red }) => blue === red;
+const squareEqualsTriangle = ({ square, triangle }) => square === triangle;
+
+const allFiguresHasSameColor = (color) => compose(propEq(color, 4), countDupes);
+const moreOrEqualsTwo = gte(__, 2);
+const greaterOrEqualsThenThree = gte(__, 3);
+const anyGreaterOrEqualsThenThree = any(greaterOrEqualsThenThree);
+const anyValueGreaterOrEqualsThenThree = compose(
+  anyGreaterOrEqualsThenThree,
+  values
+);
+
+const oneRed = propEq("red", 1);
+const twoGreens = propEq("green", 2);
+const twoGreenColors = compose(twoGreens, countDupes);
+const oneRedColor = compose(oneRed, countDupes);
+
 /**
  * @file Домашка по FP ч. 1
  *
@@ -14,37 +138,51 @@
  */
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
-
-    return star === 'red' && square === 'green';
-};
+export const validateFieldN1 = allPass([
+  isStarRed,
+  isSquareGreen,
+  isTriangleWhite,
+  isCircleWhite,
+]);
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = compose(moreOrEqualsTwo, numberOfGreenColors);
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = compose(redEqualsBlue, countDupes);
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = allPass([
+  isStarRed,
+  isSquareOrange,
+  isCircleBlue,
+]);
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = compose(
+  anyValueGreaterOrEqualsThenThree,
+  countDupesWhitoutWhite
+);
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = allPass([
+  isTriangleGreen,
+  twoGreenColors,
+  oneRedColor,
+]);
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = allFiguresHasSameColor("orange");
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = allPass([isStarNotWhite, isStarNotRed]);
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = allFiguresHasSameColor("green");
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = allPass([
+  isNotWhiteSquare,
+  isNotWhiteTriangle,
+  squareEqualsTriangle,
+]);
